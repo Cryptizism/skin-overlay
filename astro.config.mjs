601b470288583719b5731bbececed74eaeef220a
Cryptizism <cryptizism@protonmail.com>
2023-08-01 20:59:36 +0100

    Added github actions for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://cryptizism.github.io',
+  base: '/skin-overlay',
 });
